1e4f5d2a9bdf5a6e8828d8e8373baa06ff365f15
unclebill <billbill290@gmail.com>
2023-05-09 12:04:06 +0800

    avoid twitter.com redirection

--- scripts/utils.js
+++ scripts/utils.js
@@ -9,7 +9,24 @@ const prettier = require('prettier')
  * @returns Promise<string>
  */
 async function fetchFile(url) {
-  return fetch(url).then((res) => res.text())
+  return fetch(url, {
+    headers: {
+      accept:
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      'accept-language': 'en',
+      'cache-control': 'max-age=0',
+      'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'document',
+      'sec-fetch-mode': 'navigate',
+      'sec-fetch-site': 'same-origin',
+      'sec-fetch-user': '?1',
+      'upgrade-insecure-requests': '1',
+      cookie:
+        'guest_id_marketing=v1%3A168360288791376669; guest_id_ads=v1%3A168360288791376669; personalization_id="v1_DE/XcgnKUTr5KH59AAUCOg=="; guest_id=v1%3A168360289097392384; ct0=0d23fd6d647e0290345b25e206c7a5ba; _ga=GA1.2.118251776.1683602892; _gid=GA1.2.1608677838.1683602892',
+    },
+  }).then((res) => res.text())
 }
 
 /**
